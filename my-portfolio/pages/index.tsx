31e9081a850b61883f2e8717a3166468b3d42ca8
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-24 15:10:37 +0100

    Update index.tsx

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import Head from 'next/head'
+
+function IndexPage() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Portfolio Marnik Maes - Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <h1 className='text-4xl underline'>Hello world!</h1>
+    </div>
   )
 }
+
+export default IndexPage
