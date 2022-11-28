2c22d038acbf1e45c8bb013f9f786ba325fdaa85
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-28 10:57:25 +0100

    footer-changes

--- my-portfolio/pages/components/Footer.tsx
+++ my-portfolio/pages/components/Footer.tsx
@@ -7,15 +7,12 @@ function Footer(){
         <div className="absolute w-full overflow-hidden z-10">
             <div className="relative inset-y-0 bottom-0 z-10 ">
                 <footer className="bg-emerald-200 m-auto h-auto z-10">
-                    <div className="flex justify-between p-5 z-40">
-                        <div className="flex-none w-52">
-                            <div className="flex flex-col justify-between space-y-2">
-                                <h1 className="text-5xl font-Roboto">Contact</h1>
-                                <p>Marnik Maes</p>
-                            </div>
-                        </div>
+                    <div className="flex justify-between items-center md:px-[50px] md:py-[10px] py-5 px-0 z-40">
                         <div>
                             <Image src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={200} height={200}></Image> 
+                        </div>
+                        <div className='hidden xl:block'>
+                            <h1 className='text-lg font-Roboto font-semibold'>"Life is not a game of luck. If you wanne win, work hard." - Sora &#40;No Game No Life&#41;</h1>
                         </div>
                         <div className="flex-none w-52">
                             <div className='flex flex-col justify-between space-y-2 items-center'>
@@ -26,6 +23,7 @@ function Footer(){
                             </div>
                         </div>
                     </div>
+                    <div className='text-center'><p className='font-Roboto text-lg'>@2022 Marnik Maes</p></div>
                 </footer>
             </div>
 
