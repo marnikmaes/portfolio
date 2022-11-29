cbd8158836f2a550ccebf934e742296e570fab3e
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-29 12:12:49 +0100

    LogoChange-CustomColors
    LogoChange-CustomColors

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "customWhite": "#F1F1F1",
+        "customBlue": "#1C6E8C",
+        "customBlack": "#051014",
+        "customOrange": "#EE6352",
+      },
       fontFamily: {
         "Roboto" : ["Roboto", "sans-serif"],
       },
