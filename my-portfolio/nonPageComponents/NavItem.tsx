577d90e0a42e5755113cc941ec2aabbfabf51342
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-29 12:58:53 +0100

    color-change
    color-change

--- my-portfolio/nonPageComponents/NavItem.tsx
+++ my-portfolio/nonPageComponents/NavItem.tsx
@@ -13,8 +13,8 @@ const NavItem = (props: MyProps) => {
         passHref
         className={`${
             props.mobile
-            ? "font-Roboto text-customWhite flex flex-col py-4 justify-around text-center text-2xl hover:underline hover:underline-offset-8 hover:decoration-customOrange"
-            : "font-Roboto text-customBlack py-5 px-3 hover:underline hover:underline-offset-8 hover:decoration-customOrange"
+            ? "font-Roboto text-customWhite flex flex-col py-4 justify-around text-center text-2xl hover:underline hover:underline-offset-8 hover:decoration-customPurple"
+            : "font-Roboto text-customBlack py-5 px-3 hover:underline hover:underline-offset-8 hover:decoration-customPurple"
         }`}
         >
         {props.name}
--- my-portfolio/pages/components/Nav.tsx
+++ my-portfolio/pages/components/Nav.tsx
@@ -17,12 +17,10 @@ function Nav() {
             <div className="max-w-screen mx-auto px-8 h-[40px]">
                 <div className="flex justify-between py-2">
                     <div className="flex items-center space-x-1">
-                        <div className='border rounded-3xl bg-customWhite px-2'>
-                            <Link href={"/"}> 
-                                <Image className='inline' src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={100} height={100}></Image> 
-                                <span className='hidden md:inline-block text-2xl font-bold font-Roboto text-customBlack'>Portfolio Marnik Maes</span>
-                            </Link>
-                        </div>
+                        <Link href={"/"} className='border rounded-3xl text-2xl font-Roboto font-semibold px-5 bg-customWhite hover:underline hover:underline-offset-8 hover:decoration-customPurple'>
+                            <Image className='inline' src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={100} height={100}></Image> 
+                            <span>Portfolio Marnik Maes</span> 
+                        </Link>
                     </div>
                     <div className="hidden lg:flex items-center space-x-1 text-xl border rounded-3xl bg-customWhite">
                         <NavItem name="HOME" href="/" />
