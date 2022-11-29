260f16ce7b3fae03fcd2c4b0895ac397e726c164
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-29 12:25:50 +0100

    better-nav-footer
    better-nav-footer

--- my-portfolio/pages/components/Footer.tsx
+++ my-portfolio/pages/components/Footer.tsx
@@ -6,24 +6,24 @@ function Footer(){
     return (
         <div className="absolute w-full overflow-hidden z-10">
             <div className="relative inset-y-0 bottom-0 z-10 ">
-                <footer className="bg-gray-100 border border-black m-auto h-auto z-10">
+                <footer className="bg-customBlack m-auto h-auto z-10">
                     <div className="flex justify-between items-center md:px-[50px] md:py-[10px] py-5 px-0 z-40">
-                        <div>
+                        <div className='border rounded-3xl bg-customWhite'>
                             <Image src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={200} height={200}></Image> 
                         </div>
                         <div className='hidden xl:block'>
-                            <h1 className='text-lg font-Roboto font-semibold'>&#34;Life is not a game of luck. If you wanna win, work hard.&#34; &#45; Sora &#40;No Game No Life&#41;</h1>
+                            <h1 className='text-lg text-customWhite font-Roboto font-semibold'>&#34;Life is not a game of luck. If you wanna win, work hard.&#34; &#45; Sora &#40;No Game No Life&#41;</h1>
                         </div>
                         <div className="flex-none w-52">
                             <div className='flex flex-col justify-between space-y-2 items-center'>
-                                <Link href={"/"}><Icon icon="material-symbols:mail-rounded" width={40} height={40}/></Link>
-                                <Link href={"/"}><Icon icon="mdi:github" width={40} height={40}/></Link>
-                                <Link href={"/"}><Icon icon="mdi:twitter" width={40} height={40}/></Link>
-                                <Link href={"/"}><Icon icon="mdi:linkedin" width={40} height={40}/></Link>
+                                <Link href={"/"}><Icon className='text-customWhite' icon="material-symbols:mail-rounded" width={40} height={40}/></Link>
+                                <Link href={"/"}><Icon className='text-customWhite' icon="mdi:github" width={40} height={40}/></Link>
+                                <Link href={"/"}><Icon className='text-customWhite' icon="mdi:twitter" width={40} height={40}/></Link>
+                                <Link href={"/"}><Icon className='text-customWhite' icon="mdi:linkedin" width={40} height={40}/></Link>
                             </div>
                         </div>
                     </div>
-                    <div className='text-center'><p className='font-Roboto text-lg'>@2022&#45;2023 Marnik Maes</p></div>
+                    <div className='text-center text-customWhite pb-5'><p className='font-Roboto text-lg'>@2022&#45;2023 Marnik Maes</p></div>
                 </footer>
             </div>
 
--- my-portfolio/pages/components/Nav.tsx
+++ my-portfolio/pages/components/Nav.tsx
@@ -13,16 +13,18 @@ function Nav() {
       }
 
     return(
-        <nav className="bg-customBlue border border-black">
-            <div className="max-w-screen mx-auto px-5 h-[20px]">
-                <div className="flex justify-between">
+        <nav className="bg-customBlue">
+            <div className="max-w-screen mx-auto px-8 h-[40px]">
+                <div className="flex justify-between py-2">
                     <div className="flex items-center space-x-1">
-                        <Link href={"/"}> 
-                            <Image className='inline' src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={100} height={100}></Image> 
-                            <span className='hidden md:inline-block text-2xl font-bold font-Roboto text-customWhite'>Portfolio Marnik Maes</span>
-                        </Link>
+                        <div className='border rounded-3xl bg-customWhite px-2'>
+                            <Link href={"/"}> 
+                                <Image className='inline' src={"/images/MM-logo.png"} alt={"Logo Marnik"} width={100} height={100}></Image> 
+                                <span className='hidden md:inline-block text-2xl font-bold font-Roboto text-customBlack'>Portfolio Marnik Maes</span>
+                            </Link>
+                        </div>
                     </div>
-                    <div className="hidden lg:flex items-center space-x-1 text-xl">
+                    <div className="hidden lg:flex items-center space-x-1 text-xl border rounded-3xl bg-customWhite">
                         <NavItem name="HOME" href="/" />
                         <NavItem name="ABOUT ME" href="/" />
                         <NavItem name="PROJECTS" href="/" />
@@ -30,7 +32,7 @@ function Nav() {
                     </div>
                     <div className="lg:hidden flex items-center px-3 pr-5">
                         <button className="mobile-menu-button" onClick={toggleMobile}>
-                            <Icon className="w-16 h-16" icon="charm:menu-hamburger" />
+                            <Icon className="w-16 h-16 text-customWhite" icon="charm:menu-hamburger" />
                         </button>
                     </div>
                 </div>
