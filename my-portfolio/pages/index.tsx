61068ad9a8b79957f3d30bb4f8effdb9a4525a6e
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-11-29 13:15:50 +0100

    index-nav-design-changes
    index-nav-design-changes

--- my-portfolio/pages/index.tsx
+++ my-portfolio/pages/index.tsx
@@ -27,16 +27,16 @@ function IndexPage() {
 
         <div className='pt-6 flex flex-wrap justify-around space-y-[50px] md:space-y-0 md:max-w-[1720px] m-auto'>
           <div className='md:py-8'>
-            <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <div className='max-w-sm bg-customWhite rounded-lg shadow-lg dark:bg-customBlue dark:border-customBlue'>
               <Link className='' href={"/"}>
                 <Image className='rounded-lg' src={"/images/projects/Jumping-Fox-Game.png"} alt={"Jumping fox game"} height={400} width={400}></Image>
               </Link>
               <div className='p-5'>
                 <Link href={"/"}>
-                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Testing</h5>
+                  <h5 className='mb-2 text-2xl font-Roboto font-semibold tracking-tight text-customWhite dark:text-customWhite'>Jumping Fox game</h5>
                 </Link>
-                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
-                <Link className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' href={"/"}>
+                <p className='mb-3 font-normal text-customWhite dark:text-customWhite'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
+                <Link className='inline-flex items-center px-3 py-2 rounded-lg text-md font-Roboto font-semibold text-center text-customBlack bg-customWhite hover:text-customWhite hover:bg-customPurple' href={"/"}>
                   Read More
                 </Link>
               </div>
@@ -44,42 +44,41 @@ function IndexPage() {
           </div>
 
           <div className='md:py-8'>
-            <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
+            <div className='max-w-sm bg-customWhite rounded-lg shadow-lg dark:bg-customBlue dark:border-customBlue'>
               <Link className='' href={"/"}>
                 <Image className='rounded-lg' src={"/images/projects/Jumping-Fox-Game.png"} alt={"Jumping fox game"} height={400} width={400}></Image>
               </Link>
               <div className='p-5'>
                 <Link href={"/"}>
-                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Testing</h5>
+                  <h5 className='mb-2 text-2xl font-Roboto font-semibold tracking-tight text-customWhite dark:text-customWhite'>Jumping Fox game</h5>
                 </Link>
-                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
-                <Link className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' href={"/"}>
+                <p className='mb-3 font-normal text-customWhite dark:text-customWhite'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
+                <Link className='inline-flex items-center px-3 py-2 rounded-lg text-md font-Roboto font-semibold text-center text-customBlack bg-customWhite hover:text-customWhite hover:bg-customPurple' href={"/"}>
+                  Read More
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className='md:py-8'>
+            <div className='max-w-sm bg-customWhite rounded-lg shadow-lg dark:bg-customBlue dark:border-customBlue'>
+              <Link className='' href={"/"}>
+                <Image className='rounded-lg' src={"/images/projects/Jumping-Fox-Game.png"} alt={"Jumping fox game"} height={400} width={400}></Image>
+              </Link>
+              <div className='p-5'>
+                <Link href={"/"}>
+                  <h5 className='mb-2 text-2xl font-Roboto font-semibold tracking-tight text-customWhite dark:text-customWhite'>Jumping Fox game</h5>
+                </Link>
+                <p className='mb-3 font-normal text-customWhite dark:text-customWhite'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
+                <Link className='inline-flex items-center px-3 py-2 rounded-lg text-md font-Roboto font-semibold text-center text-customBlack bg-customWhite hover:text-customWhite hover:bg-customPurple' href={"/"}>
                   Read More
                 </Link>
               </div>
             </div>
           </div>
 
-          <div className='md:py-8'>
-            <div className='max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
-              <Link className='' href={"/"}>
-                <Image className='rounded-lg' src={"/images/projects/Jumping-Fox-Game.png"} alt={"Jumping fox game"} height={400} width={400}></Image>
-              </Link>
-              <div className='p-5'>
-                <Link href={"/"}>
-                  <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Testing</h5>
-                </Link>
-                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, delectus.</p>
-                <Link className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' href={"/"}>
-                  Read More
-                </Link>
-              </div>
-            </div>
-          </div>
         </div>
-
         <div className='text-center xl:text-right xl:mr-[200px] py-6'>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-[200px]">
+          <button className="font-semibold py-2 px-4 border-2 rounded-full border-customBlack  w-[200px] bg-customWhite text-customBlack hover:bg-customPurple hover:text-customWhite">
             All projects  
           </button>
         </div>
