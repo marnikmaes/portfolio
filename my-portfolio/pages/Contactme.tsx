c212a39af1e2dcad9aae7cbd3949e04be8c25ac0
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-12-02 10:54:19 +0100

    contactme-betterbackground-betterlinkbuttons
    contactme-betterbackground-betterlinkbuttons

--- my-portfolio/pages/Contactme.tsx
+++ my-portfolio/pages/Contactme.tsx
@@ -15,15 +15,17 @@ function Contactme() {
         <link rel="icon" href="images/MM-favicon.png"/>
       </Head>
 
-      <main className="bg-customBlack text-customWhite font-Roboto z-10 px-5 md:px-0">
+      <div className="bg-[url('https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] fixed z-1 block  h-full inset-0 "></div>
+
+      <main className="text-customWhite z-10 px-5 md:px-0 font-Roboto">
         <Nav/>
 
         <div className='text-center xl:text-left xl:ml-[200px] py-6'>
           <h1 className='text-5xl font-Roboto font-semibold'>Formal contact?</h1>
         </div>
 
-        <div className='flex flex-wrap justify-around space-x-1 space-y-10 md:space-y-0 py-6'>
-          <div className='w-[300px] md:w-[400px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
+        <div className='flex flex-wrap justify-around space-y-10 md:space-y-0 py-6'>
+          <div className='w-[300px] md:w-[500px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
             <Icon className='text-customWhite m-auto' icon="material-symbols:mail-rounded" width={150} height={150}/>
             <div className='px-3 pb-3'>
               <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>Email</h5>
@@ -31,21 +33,21 @@ function Contactme() {
                   I check these everyday and you can expect a response within a day or 2 at max.
                 </p>
                 <Link href={"mailto:r0881840@student.thomasmore.be"}>
-                  <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Student email</div>
+                  <div className='border-[2px] border-towerBlue  w-[200px] md:w-[250px] rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Student email</div>
                 </Link>
                 <Link href={"mailto:marnikmaes46@hotmail.com"}>
-                  <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Personal email</div>
+                  <div className='border-[2px] border-towerBlue  w-[200px] md:w-[250px] rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Personal email</div>
                 </Link>  
             </div>
           </div>
 
-          <div className='w-[300px] md:w-[400px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
+          <div className='w-[300px] md:w-[500px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
             <Icon className='text-customWhite m-auto' icon="mdi:linkedin" width={150} height={150}/>
             <div className='px-3 pb-3'>
               <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>LinkedIn</h5>
                 <p className='mb-3 font-normal text-gray-500 dark:text-gray-400'>For more professional contact LinkedIn is a good option. There you can also check my network and other important things.
                 </p>
-                <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>
+                <div className='border-[2px] border-towerBlue  w-[200px] md:w-[250px] rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>
                   <Link href={"https://www.linkedin.com/in/marnik-maes-b0aa79250/"}>Add me &#64;Marnik Maes</Link>
                 </div>  
             </div>
@@ -59,7 +61,7 @@ function Contactme() {
         </div>
         
         <div className='flex flex-wrap justify-around space-x-1 space-y-10 md:space-y-0 py-6'>
-          <div className='w-[300px] md:w-[400px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
+          <div className='w-[300px] md:w-[500px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
             <Icon className='text-customWhite m-auto' icon="ic:baseline-discord" width={150} height={150}/>
             <div className='px-3 pb-3'>
               <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>Discord</h5>
@@ -70,31 +72,20 @@ function Contactme() {
                 <p className='mb-3 font-normal text-gray-500 dark:text-gray-400'>
                   &#40;Down below you can find my Discord user name, copy this and paste it in the add friend section on Discord it.&#41; 
                 </p>
-                <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>
+                <div className='border-[2px] border-towerBlue  w-[200px] md:w-[250px] rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>
                   <p>noob4head#3513</p>
                 </div>
             </div>
           </div>
 
-          <div className='w-[300px] md:w-[400px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
+          <div className='w-[300px] md:w-[500px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
             <Icon className='text-customWhite m-auto' icon="mdi:twitter" width={150} height={150}/>
             <div className='px-3 pb-3'>
               <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>Twitter</h5>
               <p className='mb-3 font-normal text-gray-500 dark:text-gray-400'>Twitter is the social media I mainly use for school. I&#39;m often active here and you can follow my Thomas More experiences.</p>
               <Link href={"https://twitter.com/MaesMarnik"}>
-                <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Add me &#64;MaesMarnik</div>
+                <div className='border-[2px] border-towerBlue w-[200px] md:w-[250px] rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Add me &#64;MaesMarnik</div>
               </Link>   
-            </div>
-          </div>
-
-          <div className='w-[300px] md:w-[400px] bg-white border border-gray-200 rounded-lg shadow-md dark:bg-skyBlue dark:border-skyBlue flex flex-col justify-end'>
-            <Icon className='text-customWhite m-auto' icon="mdi:instagram" width={150} height={150}/>
-            <div className='px-3 pb-3'>
-              <h5 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>Instagram</h5>
-              <p className='mb-3 font-normal text-gray-500 dark:text-gray-400'>Instagram is one of my lesser used social media, but I still check up on it daily. I would suggest only using this if you cannot reach me through one of my other social media.</p>
-              <Link href={"https://www.instagram.com/marnik_maes/"}>
-                <div className='border-[2px] border-towerBlue w-full rounded-full m-auto bg-white text-center text-customBlack hover:bg-waterBlue hover:text-customWhite'>Add me &#64;marnik_maes</div>
-              </Link>  
             </div>
           </div>
         </div>
