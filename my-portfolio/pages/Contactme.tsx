316f636ec5dc425d1561c6d3b487a1f6954c8c14
Marnik Maes <91118430+marnikmaes@users.noreply.github.com>
2022-12-07 09:41:41 +0100

    footerFix-muchBetterBackground
    footerFix-muchBetterBackground

--- my-portfolio/pages/Contactme.tsx
+++ my-portfolio/pages/Contactme.tsx
@@ -14,11 +14,11 @@ function Contactme() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
         <link rel="icon" href="images/MM-favicon.png"/>
       </Head>
-      
-      <div className='min-h-screen'>
-        <main className="bg-customBlack text-customWhite font-Roboto z-10 px-5 md:px-0">
-          <Nav/>
 
+      <div className="bg-customBlack xl:bg-[url('https://img.freepik.com/free-vector/matrix-style-binary-code-digital-falling-numbers-blue-background_1017-37387.jpg?w=2000&t=st=1670401340~exp=1670401940~hmac=00b0ab701b3009d502016df7b06fa5079e9e0284809a7698ee42e855df8f3937')] fixed z-1 block  h-full inset-0 "></div>
+      <div className='min-h-screen'>
+        <main className="text-customWhite z-10 px-5 md:px-0 font-Roboto">
+          <Nav/>
           <div className='text-center xl:text-left xl:ml-[200px] py-6'>
             <h1 className='text-5xl font-Roboto font-semibold'>Formal contact?</h1>
           </div>
--- my-portfolio/pages/Projects.tsx
+++ my-portfolio/pages/Projects.tsx
@@ -14,8 +14,10 @@ function Projects() {
         <link rel="icon" href="images/MM-favicon.png"/>
       </Head>
 
+      
+      <div className="bg-customBlack xl:bg-[url('https://img.freepik.com/free-vector/matrix-style-binary-code-digital-falling-numbers-blue-background_1017-37387.jpg?w=2000&t=st=1670401340~exp=1670401940~hmac=00b0ab701b3009d502016df7b06fa5079e9e0284809a7698ee42e855df8f3937')] fixed z-1 block  h-full inset-0 "></div>
       <div className='min-h-screen'>
-        <main className="bg-customBlack text-customWhite font-Roboto z-10 px-5 md:px-0">
+        <main className="text-customWhite z-10 px-5 md:px-0 font-Roboto">
           <Nav/>
         </main>
         <Footer/>
